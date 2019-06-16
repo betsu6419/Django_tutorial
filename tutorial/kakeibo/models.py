5ee6bc8a665feb1f3672a13d2aba6010552c41b2
betsu6419 <yusawabe@gmail.com>
2019-06-16 22:04:18 +0900

    a

---
+++ tutorial/kakeibo/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+class Category(models.Model):
+    class Meta:
+        db_table = "category"
+    category_name = models.CharField(max_length = 255,unique = True)
+
+class Kakeibo(models.Model):
+    class Meta:
+        db_table = "kakeibo"
+    date = models.DateField(verbose_name = '日付',default = datetime.now)
+    category = models.ForeignKey(Category,on_delete = models.PROTECT,verbose_name = "カテゴリ")
+    money = models.IntegerField(verbose_name="金額",help_text="単位は日本円")
+    memo = models.CharField(verbose_name="メモ",max_length = 500)
---
+++ venv/tutorial/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yuuki/Documents/Django_tutorial/venv/tutorial/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
