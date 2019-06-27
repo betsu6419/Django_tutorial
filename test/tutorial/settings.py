dec05c15fb0af43c702eb4e16d6416dbf5f474cf
betsu6419 <yusawabe@gmail.com>
2019-06-27 20:22:16 +0900

    A

--- test/tutorial/settings.py
+++ test/tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ds3uyuxicg#vqlii&*4fy976^yug1dt#z93d63m*xwdwtqlsuw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.112.244.216']
 
 
 # Application definition
