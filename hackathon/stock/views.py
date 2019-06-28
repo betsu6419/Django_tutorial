f018ac6118fc38e3fce4947fae0c286d0c026c80
wadadon148 <52285595+wadadon148@users.noreply.github.com>
2019-06-28 23:27:17 +0900

    wadadon2
    web上のUIや機械学習の表示などを変更しました

--- hackathon/stock/views.py
+++ hackathon/stock/views.py
@@ -3,6 +3,7 @@ from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .models import Stock
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import datetime
 
 
 
@@ -35,6 +36,9 @@ def show_line_grahp(request):
             if j.date.strftime('%Y/%m/%d')[:10]==x_label[i]:   
                 ln.append(j.money)
        daily_min_data.append([x_label[i], min(ln)])
+   ld=len(x_label)-1
+   nowweight=daily_min_data[ld][1]
+
     
      #折れ線グラフのボーダーカラー色の設定      
    border_color=[['54,164,235,0.8']]  
@@ -44,6 +48,7 @@ def show_line_grahp(request):
        'border_color': border_color,
        'background_color': background_color,
        'matrix_list': daily_min_data,
+       'nowweight' : nowweight
                 } )
    
 def show_learn_grahp(request):
@@ -52,33 +57,40 @@ def show_learn_grahp(request):
    stock_data = Stock.objects.all()
    date_list=[]
    for i in stock_data:
-        date_list.append((i.date.strftime('%Y/%m/%d')[:10]))
-        date_list.sort()
-   x_label = list(set(date_list))
-   x_label.sort(reverse=False)
+        date_list.append((i.date))
+   redays= list(set(date_list))
+   redays.sort(reverse=False)
     #日毎の最小質量のデータセットの生成
    daily_min_data =[]
-   for i in range(len(x_label)):
+   for i in range(len(redays)):
        ln=[]
        for j in stock_data:
-            if j.date.strftime('%Y/%m/%d')[:10]==x_label[i]:   
+            if j.date==redays[i]:   
                 ln.append(j.money)
-       daily_min_data.append([x_label[i], min(ln)])
+       daily_min_data.append([redays[i], min(ln)])
    mass=[]
    for i,j in daily_min_data:
        mass.append(j)
-   t_2 = np.array(range(len(x_label)))
+   t_2 = np.array(range(len(redays)))
    mass_2 = np.array(mass)
    lr = LinearRegression()
    t_2 = t_2.reshape(-1,1)
    mass_2 = mass_2.reshape(-1,1)
    lr.fit(t_2,mass_2)
    min_t = min(t_2)
-   x = np.arange(min_t,min_t+100,1).reshape(-1,1)
+   x = np.arange(min_t,min_t+1000,1).reshape(-1,1)
    y = lr.predict(x)
-   for i in range(len(x_label)):
-      daily_min_data[i][1]=y[i]
-    
+   j=0
+   firstday=date_list[0]
+   x_label=[]
+   daily_min_data =[]
+   while y[j]>0:
+       dayj=firstday+datetime.timedelta(days=j)
+       x_label.append(dayj.strftime('%Y/%m/%d')[:10])
+       daily_min_data.append([dayj.strftime('%Y/%m/%d')[:10],y[j]])
+       j+=1
+   deadline=firstday+datetime.timedelta(days=j)
+   deadline.strftime('%Y/%m/%d')[:10]
      #折れ線グラフのボーダーカラー色の設定      
    border_color=[['54,164,235,0.8']]  
    background_color=[['54,164,235,0.5']]
@@ -87,6 +99,7 @@ def show_learn_grahp(request):
        'border_color': border_color,
        'background_color': background_color,
        'matrix_list': daily_min_data,
+       'deadline': deadline
                 } )
    
   
