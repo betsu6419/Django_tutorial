1ebb80dd51c7ef42f8892e6cd3cfed9fd648e9e3
betsu6419 <yusawabe@gmail.com>
2019-08-18 18:40:21 +0900

    bug

--- hackathon/stock/models.py
+++ hackathon/stock/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from datetime import datetime
 
  
+class Category(models.Model):
+    class Meta:
+        verbose_name = "カテゴリ"
+        verbose_name_plural = "カテゴリ"
+    
+    category_name = models.CharField(max_length = 255,unique = True)
+    def __str__(self):
+        return self.category_name
+
 class Stock(models.Model):
    class Meta:
        #テーブル名
@@ -14,5 +23,6 @@ class Stock(models.Model):
    #カラムの定義
    date = models.DateField(verbose_name="日付",default=datetime.now)
    money = models.IntegerField(verbose_name="残量", help_text="単位はグラム")
+   category = models.CharField(verbose_name = "カテゴリ",max_length = 255,unique = True)
 
     
--- hackathon/stock/views.py
+++ hackathon/stock/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.conf import settings
-from .models import Stock
+from .models import Stock ,Category
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import datetime
@@ -46,7 +46,7 @@ def Update_from_dDB():
         print(created)
         print("#########")
     
-    
+
 
 #一覧表示用のDjango標準ビュー(ListView)を承継して一覧表示用のクラスを定義
 class StockListView(ListView):
@@ -66,37 +66,71 @@ def show_line_grahp(request):
    
    #全データ取得
    Update_from_dDB()
-
    stock_data = Stock.objects.all()
+   category_list = []
+    category_data = Category.objects.all().order_by('-category_name')
+    for item in category_data:
+        category_list.append(item.category_name)
+
    date_list=[]
    for i in stock_data:
         date_list.append((i.date.strftime('%Y/%m/%d')[:10]))
         date_list.sort()
    x_label = list(set(date_list))
    x_label.sort(reverse=False)
-    #日毎の最小質量のデータセットの生成
-   daily_min_data =[]
-   for i in range(len(x_label)):
-       ln=[]
-       for j in stock_data:
-            if j.date.strftime('%Y/%m/%d')[:10]==x_label[i]:   
-                ln.append(j.money)
-       daily_min_data.append([x_label[i], ])
-   ld=len(x_label)-1
-   nowweight=daily_min_data[ld][1]
 
+   #月毎＆カテゴリ毎の合計金額データを生成する。
+    #カテゴリが登録されていない月の合計金額は０にセットする。
+    #まず、全日付ｘ全カテゴリｘ合計金額「0]のリストを生成する。
+    matrix_list =[]
+    for item_label in x_label:
+        for item_category in category_list:
+            matrix_list.append([item_label, item_category, 0])
+
+    #日毎の最小質量のデータセットの生成
+    nowweight = []
+    daily_min_data =[]
+    for i,category in enumerate(category_list):
+        for i in range(len(x_label)):
+            ln=[]
+            for j in stock_data:
+                if j.category == category:
+                    if j.date.strftime('%Y/%m/%d')[:10]==x_label[i]:   
+                        ln.append(j.money)
+            daily_min_data.append([x_label[i],category, ln[-1]])
+        ld=len(x_label)-1
+        nowweight.append(daily_min_data[ld][2])
+
+    for x,category,weight in daily_min_data:
+        for i data in enumerate(matrix_list):
+            if data[0] == x and data[1] == category:
+                matrix_list[i][2] = weight
     
-     #折れ線グラフのボーダーカラー色の設定      
-   border_color=[['54,164,235,0.8']]  
-   background_color=[['54,164,235,0.5']]
+     #折れ線グラフのボーダーカラー色の設定        #折れ線グラフのボーダーカラー色の設定      
+    border_color_list=['254,97,132,0.8','54,164,235,0.8','0,255,65,0.8','255,241,15,0.8',\
+                       '255,94,25,0.8','84,77,203,0.8','204,153,50,0.8','214,216,165,0.8',\
+                       '33,30,45,0.8','52,38,89,0.8']
+    border_color =[]
+    for x,y in zip(category_list, border_color_list):
+        border_color.append([x,y])
+        
+    background_color_list=['254,97,132,0.5','54,164,235,0.5','0,255,65,0.5','255,241,15,0.5',\
+                           '255,94,25,0.5','84,77,203,0.5','204,153,50,0.5','214,216,165,0.5'\
+                           '33,30,45,0.5','52,38,89,0.5']
+
+    background_color =[]
+    for x,y in zip(category_list, background_color_list):
+        background_color.append([x,y])
+    
    return render(request,'stock/stock_line.html',{
        'x_label': x_label,
+       'category_list':category_list,
        'border_color': border_color,
        'background_color': background_color,
-       'matrix_list': daily_min_data,
+       'matrix_list': matrix_list,
        'nowweight' : nowweight
                 } )
-   
+
 def show_learn_grahp(request):
    Update_from_dDB
 
@@ -114,7 +148,7 @@ def show_learn_grahp(request):
        for j in stock_data:
             if j.date==redays[i]:   
                 ln.append(j.money)
-       daily_min_data.append([redays[i], ln[-1])
+       daily_min_data.append([redays[i], ln[-1]])
    mass=[]
    for i,j in daily_min_data:
        mass.append(j)
@@ -148,5 +182,6 @@ def show_learn_grahp(request):
        'matrix_list': daily_min_data,
        'deadline': deadline
                 } )
+
    
   
